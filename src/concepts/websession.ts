c02083a5faa8da3fec21832d2357cb4249a41c48
Grace <gsjau@mit.edu>
2023-09-20 23:21:09 -0400

    prep 2: completed

--- src/concepts/websession.ts
+++ src/concepts/websession.ts
@@ -28,6 +28,9 @@ export default class WebSessionConcept {
     // Hint: Take a look at how the "end" function makes sure the user is logged in. Keep in mind that a
     // synchronization like starting a session should just consist of a series of actions that may throw
     // exceptions and should not have its own control flow.
+    if (session.user !== undefined) {
+      throw new UnauthenticatedError("Already logged in!");
+    }
     session.user = username;
   }
 
